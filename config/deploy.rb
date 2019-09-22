16a74515e4349c893feb7d527719075cf7c88653
Rainey <23113728+raineydavid@users.noreply.github.com>
2019-09-22 03:38:34 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,10 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :application_name, 'codingcoach'
-set :repository, 'https://github.com/Coding-Coach/coding-coach.git'
-set :domain, 'blog.codingcoach.io'
-set :deploy_to, '/var/www/codingcoach.io'
+set :application_name, 'womencancode'
+set :repository, 'https://github.com/Womencancode/womentors.git'
+set :domain, 'blog.womencancode.com'
+set :deploy_to, '/var/www/cwomencancode.com'
 set :branch, 'master'
 
 # Optional settings:
